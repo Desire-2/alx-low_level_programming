553f7c436117ff68d0541ef046aac86d375953f7
Desire-2 <bikorimanadesire5@gmail.com>
2023-02-16 14:01:08 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-
-
 /**
- * main - Prints the size of various typesa based on
- *  the computer it is compiled and run on..
- *  Return: Always 0.
+ * main - A program that Prints the size of various computer types
+ *  Return: 0 (Sucess)
  */
 int main(void)
 {
