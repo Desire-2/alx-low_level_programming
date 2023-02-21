11e6ea101e8d6b4f33558165ca371833a510279e
Desire-2 <bikorimanadesire5@gmail.com>
2023-02-21 11:22:36 +0200

    0-putchar.c 1-alphabet.c 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,19 @@
-#include <stdio.h>
-/*" This a program that prints _putchar, followed by a new line"*/
+#include "main.h"
+/** a program that prints _putchar, followed by a new line
+ * by Desire
+ * return:0
+ */
+
 
 int main(void)
 {
-	printf(_putchar\n)
+	_putchar('_')
+		_putchar('d');
+		_putchar('e');
+		_putchar('s');
+		_putchar('i');
+		_putchar('r');
+		_putchar('e');
+		_putchar('\n');
+		return (0);
 }
-		
