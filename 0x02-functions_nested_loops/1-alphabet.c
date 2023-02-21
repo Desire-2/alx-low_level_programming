01afc29db9e4e2a0eb89bc8ba953ef1c693688f8
Desire-2 <bikorimanadesire5@gmail.com>
2023-02-21 11:57:06 -0800

    1-alphabet.c 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "main.h"
-
 /**
- *main - check the code
- *Return: Always 0.
-*/
-int main(void)
+ * print_alphabet - It's a function for prints alphabetic
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,25 @@
-vi
+#include "main.h"
+/**
+ * print_alphabet_x10 - function definition
+ *
+ * Description: A program that print alphabet 10x
+ *
+ * By Desire
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+
+		i++;
+	}
+}
