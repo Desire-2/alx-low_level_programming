10be0bb83f15a2ab797692c17f581a1cdfabea06
Desire-2 <bikorimanadesire5@gmail.com>
2023-02-21 03:39:27 -0800

    ./0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,17 @@
 */
 
 
-int _putchar(char c);
+int  main(void)
 {
 	_putchar('_');
-		_putchar('d');
-		_putchar('e');
-		_putchar('s');
-		_putchar('i');
-		_putchar('r');
-		_putchar('e');
-		_putchar('\n');
-		return (0);
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
 
