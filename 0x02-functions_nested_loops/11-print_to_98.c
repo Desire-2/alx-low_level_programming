e177cc25e39a3a7ce6847644774c135cdbc09423
Desire-2 <bikorimanadesire5@gmail.com>
2023-02-22 10:03:19 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,38 +1,42 @@
 #include "main.h"
-/**
- * print_to_98 - Function definition
- *
- * Description: A program that print all natural number
- * from n to 98
- * @n: paramenter that accept integer
- *
- * Return: result
- */
-void print_to_98(int n)
-{
-	int i;
 
-	if (n <= 98)
+/**
+ * times_table - prints the 9 times table
+ * Description: prints the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int row, column, product, tens, ones;
+
+	for (row = 0; row <= 9; row++)
 	{
-		for (i = n; i <= 98; i++)
+		for (column = 0; column <= 9; column++)
 		{
-			printf("%d", i);
-			if (i == 98)
-				break;
-			printf(",");
-			printf(" ");
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
+		_putchar('\n');
 	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i == 98)
-				break;
-			printf(",");
-			printf(" ");
-		}
-	}
-	printf("\n");
 }
