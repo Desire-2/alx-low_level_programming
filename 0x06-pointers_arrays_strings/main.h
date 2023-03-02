c14565fe767b42130f3911db366b6949e6aa399d
Desire-2 <bikorimanadesire5@gmail.com>
2023-03-02 01:14:33 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
+void print_number(int n);
 char *cap_string(char *s);
 char *leet(char *s);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_number(int n);
