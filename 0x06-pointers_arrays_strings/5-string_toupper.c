1de185affe7281c6d69f0a3b263418cfa2342df3
Desire-2 <bikorimanadesire5@gmail.com>
2023-03-05 08:15:07 -0800

    A file contain a function that changes all lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -17,7 +17,8 @@ char *string_toupper(char *s)
 	while (s[i] != '\0')
 	{
 		if (s[i] >= 97 && s[i] <= 122)
-			s[i] = s[i] - 32;
+			s[i] = s[i] - 32;/*The ASCII codes for a-z are 97-122*/
+					   /*The ASCII codes for A-Z are 65-90.*/
 		i++;
 	}
 	return (s);
