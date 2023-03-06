af23f5be8da7d0732201541fee94f4f07fb0b3b8
Desire-2 <bikorimanadesire5@gmail.com>
2023-03-06 08:41:56 -0800

    A file contain a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,34 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - a function that fills
- *         memory with a constant byte
- *
- * @s: input pointer to char type
- *    represents the pointer to the
- *    block of memory to fill
- * @b: input variable of char type
- *    represents the character to
- *    fill s
- * @n: unsigned int variable
- *    the number of bytes to be filled
- *
- * Return: A pointer to the filled memory
- *         area @s
-*/
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: max bytes to use
+ * Return: s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	/**
-	 * declare an unsigned int
-	 * because we are storing a
-	 * value that will always be
-	 * non-negative (zero or positive)
-	*/
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; n > 0; i++, n--)
+	{
 		s[i] = b;
+	}
 
 	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
@@ -18,3 +22,6 @@ void print_diagsums(int *a, int size);
 
 void set_string(char **s, char *to);
 
+int _putchar(char c);
+
+#endif
