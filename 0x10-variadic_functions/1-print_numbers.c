f298ff8739846677cf68a41f56657f748351724b
Desire-2 <bikorimanadesire5@gmail.com>
2023-03-23 12:07:20 -0700

    A file that contain a function that prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print_numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list night;
+	unsigned int a:
+
+		va_start(night, n);
+	for (a = 0; a < n; a++)/*loop a*/
+	{
+		printf("%d", va_arg(night, int));
+
+		if (separator && a < n - 1)
+			printf("%s", separator)
+	}
+	printf("\n");
+	va_end(night);
+}
+
