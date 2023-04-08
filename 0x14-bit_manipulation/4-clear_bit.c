ea20bb15cc7afa96973fcac88f686f9a4a29f115
Desire-2 <bikorimanadesire5@gmail.com>
2023-04-08 06:40:08 -0700

    -m

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a gin index.
+ * @index: index to start.
+ * @n: number of bit to set.
+ * Return: 1 on success, -1 fail
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index);
+{
+	unsigned int r;
+
+	if (index > 63)
+		return (-1);
+
+	r = 1 << index;
+	if (*n & r)
+		*n ^= m;
+	return (1);
+}
