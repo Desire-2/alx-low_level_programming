e7b846a9530505dd0a626edad0d3f6681ffcd942
root <Desire-2>
2023-05-04 05:09:06 -0700

    Function that prints the binary representation of a number.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -20,27 +20,3 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 	}
 }
-/**
- * main - main call
- * Return: ......
- */
-int main(void)
-{
-	print_binary(0);
-	_putchar('\n');
-
-	print_binary(1);
-	_putchar('\n');
-
-	print_binary(98);
-	_putchar('\n');
-
-	print_binary(1024);
-	_putchar('\n');
-
-	print_binary(1025);
-	_putchar('\n');
-
-	return (0);
-}
-
