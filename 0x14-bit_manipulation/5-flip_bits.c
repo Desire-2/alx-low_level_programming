c4568025de95fc1c46960afce16f4720c2bf2107
Desire-2 <bikorimanadesire5@gmail.com>
2023-07-10 15:31:16 -0700

    Implement flip_bits function for calculating the number of bits to flip between two numbers

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - returns the number of bits you would need to flip to
+ * get from one number to another.
+ * @n: unsigned long int
+ * @m:unsigned long int
+ * Return: void
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_r = n ^ m;
+	unsigned int cnt = 0;
+
+	while (xor_r != 0)
+	{
+		cnt++;
+		xor_r &= (xor_r - 1);
+	}
+	return (cnt);
+}
+
