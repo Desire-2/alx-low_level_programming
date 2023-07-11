6c8c84c7deef7b06395b8c2d52f15144979bcaf2
Desire-2 <bikorimanadesire5@gmail.com>
2023-07-11 05:24:10 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -25,8 +25,7 @@ int open_file(const char *path, int flags, mode_t mode);
 void copy_file(int fd_from, int fd_to);
 void close_file(int fd);
 
-
-
+/*
 void verify_elf(unsigned char *e_ident);
 void print_m(unsigned char *e_ident);
 void print_c(unsigned char *e_ident);
@@ -37,6 +36,25 @@ void print_oper(unsigned char *e_ident);
 void print_type_elf(unsigned int e_type, unsigned char *e_ident);
 void print_entry_pnt(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf_file(int elf);
+*/
 
+void check_elf(unsigned char *e_ident);
 
+void print_magic(unsigned char *e_ident);
+
+void print_class(unsigned char *e_ident);
+
+void print_data(unsigned char *e_ident);
+
+void print_version(unsigned char *e_ident);
+
+void print_abi(unsigned char *e_ident);
+
+void print_osabi(unsigned char *e_ident);
+
+void print_type(unsigned int e_type, unsigned char *e_ident);
+
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+
+void close_elf(int elf);
 #endif
