2b9b4961f80bbcf29a3af22dae14bc3f27161a5b
root <Desire-2>
2023-09-04 14:26:38 -0700

    Program that copies the content of a file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,12 +2,14 @@
 #define MAIN_H
 
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
+void close_file(int FD);
 int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
