c4a88d21e2161b24aeabd5c599451aa0fd60e523
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-16 12:18:57 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/Desire-2/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD-PRELOAD=/tmp/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
