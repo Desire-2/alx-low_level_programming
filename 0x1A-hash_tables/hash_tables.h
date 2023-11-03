500abb30353530fec07d2ea0013f6cc4c8baf11a
Desire-2 <bikorimanadesire5@gmail.com>
2023-11-03 12:59:46 -0700

    Header file

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,38 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct hash_node_s - Hash table Node
+ *
+ * @key: The key, string
+ * @value: Value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash Table Data Structure
+ * @size: Array size
+ * @array: An array of size @size
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+#endif
